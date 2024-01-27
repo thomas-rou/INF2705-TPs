f144a17bc304deddeb5ebee9f7311cacd73c8aca
Thomas Rouleau <thomas.rouleau@polymtl.ca>
2024-01-27 18:16:17 -0500

    tp1_H24/src/shaders: completed transform vertex and fragment shaders
    transform.vs.glsl: similar to color.vs.glsl but with a uniform matrix to
    modify the position
    
    transform.fs.glsl: same as color.fs.glsl

---
+++ tp1_H24/src/shaders/transform.vs.glsl
@@ -0,0 +1,15 @@
+#version 330 core
+
+
+layout(location = 0) in vec3 a_position;
+layout(location = 1) in vec3 a_color;
+
+uniform mat4 mvpMatrix;
+
+out vec3 color;
+
+void main()
+{
+    gl_Position = mvpMatrix * vec4(a_position, 1.0);
+    color = a_color;
+}
