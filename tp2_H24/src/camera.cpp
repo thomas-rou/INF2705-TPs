17d16d6c4b4e92152d870b9dbdfdc54dd522e3cb
Thomas Rouleau <thomas.rouleau@polymtl.ca>
2024-02-09 11:23:52 -0500

    tp2_H24/src: Implement first-person view matrix calculation in Camera class

--- tp2_H24/src/camera.cpp
+++ tp2_H24/src/camera.cpp
@@ -3,20 +3,26 @@
 #include <math.h>
 #include <glm/gtc/matrix_transform.hpp>
 
-Camera::Camera(const glm::vec3& position, const glm::vec2& orientation)
+
+Camera::Camera(const glm::vec3& position, const glm::vec2& orientation) : m_position(position), m_orientation(orientation)
 {
-	// TODO: Initialisation des attributs de la classe
+	// Initialisation des attributs de la classe
 }
 
 glm::mat4 Camera::getFirstPersonViewMatrix()
 {
-	// TODO: Calcul et retour de la matrice de vue en première personne
+	// Calcul et retour de la matrice de vue en première personne
 	// à partir des attributs de la classe
+	glm::mat4 view = glm::mat4(1.0f);
+	view = glm::rotate(view, m_orientation.y, glm::vec3(1.0f, 0.0f, 0.0f));
+	view = glm::rotate(view, m_orientation.x, glm::vec3(0.0f, 1.0f, 0.0f));
+	view = glm::translate(view, -m_position);
+	return view;
 }
 
-glm::mat4 Camera::getThirdPersonViewMatrix()
-{
-	// TODO: Calcul et retour de la matrice de vue en troisième personne
-	// à partir des attributs de la classe
-}
+// glm::mat4 Camera::getThirdPersonViewMatrix()
+// {
+// 	// TODO: Calcul et retour de la matrice de vue en troisième personne
+// 	// à partir des attributs de la classe
+// }
 
