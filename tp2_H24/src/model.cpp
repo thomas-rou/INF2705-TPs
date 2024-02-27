9b2b7a9733b62f4de1558edfd6656b233053156f
Thomas Rouleau <thomas.rouleau@polymtl.ca>
2024-02-27 15:04:48 -0500

    tp2_H24/src/model.cpp: Update attribute stride in Model class
    loadObj function now also load texture vector into vertexData, the  stride needs to take the vec2 into account

--- tp2_H24/src/model.cpp
+++ tp2_H24/src/model.cpp
@@ -10,7 +10,7 @@ Model::Model(const char* path)
 	std::vector<GLuint> indices;
 	loadObj(path, vertexData, indices);
 	m_shape.setData(vertexData.data(), vertexData.size(), indices.data(), indices.size());
-	m_shape.enableAttribute(0, 3, 3, 0);
+	m_shape.enableAttribute(0, 3, 5, 0);
 	m_shape.enableAttribute(1, 2, 5, 3);
 	m_count = indices.size();
 }
@@ -32,9 +32,9 @@ void Model::loadObj(const char* path, std::vector<GLfloat>& vertexData, std::vec
 		vertexData.push_back(p.Y);
 		vertexData.push_back(p.Z);
 		// TODO: Décommenter lors de la partie 2
-		//objl::Vector2 t = loader.LoadedVertices[i].TextureCoordinate;
-		//vertexData.push_back(t.X);
-		//vertexData.push_back(t.Y);
+		objl::Vector2 t = loader.LoadedVertices[i].TextureCoordinate;
+		vertexData.push_back(t.X);
+		vertexData.push_back(t.Y);
 	}
 	indices = loader.LoadedIndices;
 }
