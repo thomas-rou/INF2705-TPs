bacc33d004416b25a98f00e845f370bc1db9c53a
Thomas Rouleau <thomas.rouleau@polymtl.ca>
2024-02-27 15:02:20 -0500

    tp2_H24/src/shaders: Update model vertex and fragments shaders to use texture mapping

--- tp2_H24/src/shaders/model.fs.glsl
+++ tp2_H24/src/shaders/model.fs.glsl
@@ -1,10 +1,13 @@
 #version 330 core
 
-uniform vec3 color;
+in vec2 vTex;
+
+uniform sampler2D tex;
 
 out vec4 FragColor;
 
 void main()
 {
-    FragColor = vec4(color, 1.0f);
+    FragColor = texture(tex, vTex);
+    if (FragColor.a < 0.3) discard;
 }
