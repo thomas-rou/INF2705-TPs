779dbacff677f6571d877aa562a0b886f66db442
Thomas Rouleau <thomas.rouleau@polymtl.ca>
2024-02-27 15:02:20 -0500

    tp2_H24/src/shaders: Update model vertex and fragments shaders to use texture mapping

--- tp2_H24/src/shaders/model.vs.glsl
+++ tp2_H24/src/shaders/model.vs.glsl
@@ -1,10 +1,14 @@
 #version 330 core
 
 layout (location = 0) in vec3 inPosition;
+layout (location = 1) in vec2 inTex;
 
 uniform mat4 mvp;
+
+out vec2 vTex;
 
 void main()
 {
     gl_Position = mvp * vec4(inPosition, 1.0);
+    vTex = inTex.st;
 }
