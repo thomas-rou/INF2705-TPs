0bbef3891068048f8db8377792f6108093802d4e
Thomas Rouleau <thomas.rouleau@polymtl.ca>
2024-02-28 17:02:14 -0500

    tp2_H24/src/camera.cpp: fix comments

--- tp2_H24/src/camera.cpp
+++ tp2_H24/src/camera.cpp
@@ -13,7 +13,6 @@ glm::mat4 Camera::getFirstPersonViewMatrix()
 {
 	// Calcul et retour de la matrice de vue en première personne
 	// à partir des attributs de la classe
-
 	float yaw = glm::radians(m_orientation.x);
 
 	glm::mat4 view = glm::mat4(1.0f);
@@ -28,11 +27,12 @@ glm::mat4 Camera::getFirstPersonViewMatrix()
 
  glm::mat4 Camera::getThirdPersonViewMatrix()
  {
- 	// TODO: Calcul et retour de la matrice de vue en troisième personne
+ 	// Calcul et retour de la matrice de vue en troisième personne
  	// à partir des attributs de la classe
 	 float radius = 6.0f;
 	 float yaw = glm::radians(m_orientation.x);
 	 float pitch = glm::radians(m_orientation.y);
+
 	 glm::vec3 cameraPosition = glm::vec3(
 		 radius * glm::cos(pitch) * glm::sin(-yaw),
 		 radius * glm::sin(pitch),
