60479df2dbb6a179a473a0a6e2af6ee772f2a34b
Thomas Rouleau <thomas.rouleau@polymtl.ca>
2024-03-22 09:14:54 -0400

    Tp3_H24: initial commit + loading of monkeys and glass

---
+++ tp3_H24/src/shaders/simple.vs.glsl
@@ -0,0 +1,13 @@
+#version 330 core
+
+layout (location = 0) in vec3 position;
+layout (location = 2) in vec3 normal;
+
+uniform mat4 mvp;
+
+void main()
+{
+    vec3 normal_unit = normalize(normal);
+    vec3 pos = position + 0.1 * normal_unit;
+    gl_position = mvp * vec4(pos, 1.0);
+}
