04c393f07a458e997d79017c6188cbfdb02a3f1b
Omar Benzekri <omar.benzekri.2003@gmail.com>
2024-04-17 23:31:07 -0400

    OB | tessellation/fragment shader

--- tp4/src/shaders/tessellation.fs.glsl
+++ tp4/src/shaders/tessellation.fs.glsl
@@ -35,7 +35,31 @@ const vec3 PATCH_EDGE_COLOR = vec3(1.0f, 0.0f, 0.0f);
 const float WIREFRAME_WIDTH = 0.5f;
 const float PATCH_EDGE_WIDTH = 0.5f;
 
-void main()
-{
-	// TODO
+void main() {
+    vec4 sandTex = texture(sandSampler, attribIn.texCoords);
+    vec4 groundTex = texture(groundSampler, attribIn.texCoords);
+    vec4 snowTex = texture(snowSampler, attribIn.texCoords);
+
+    FragColor = calculateColor(sandTex, groundTex, snowTex, attribIn.height);
+
+    if (viewWireframe) {
+        applyWireframeEffect();
+    }
+}
+
+vec4 calculateColor(vec4 sandTex, vec4 groundTex, vec4 snowTex, float height) {
+    if (height < 0.6f) {
+        float f = smoothstep(0.3f, 0.35f, height);
+        return mix(sandTex, groundTex, f);
+    } else {
+        float f = smoothstep(0.6f, 0.65f, height);
+        return mix(groundTex, snowTex, f);
+    }
+}
+
+void applyWireframeEffect() {
+    float edge = edgeFactor(attribIn.barycentricCoords, WIREFRAME_WIDTH);
+    float patchEdge = edgeFactor(attribIn.patchDistance, PATCH_EDGE_WIDTH);
+    FragColor.rgb = mix(FragColor.rgb, WIREFRAME_COLOR, 1 - edge);
+    FragColor.rgb = mix(FragColor.rgb, PATCH_EDGE_COLOR, 1 - patchEdge);
 }
