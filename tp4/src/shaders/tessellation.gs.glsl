dcc74a87a81b9e891329dba5c3ac2435777ac40a
Omar Benzekri <omar.benzekri.2003@gmail.com>
2024-04-17 23:31:32 -0400

    OB | tessellation/geometry shader

--- tp4/src/shaders/tessellation.gs.glsl
+++ tp4/src/shaders/tessellation.gs.glsl
@@ -19,7 +19,25 @@ out ATTRIB_GS_OUT
     vec3 barycentricCoords;
 } attribOut;
 
-void main()
-{
-    // TODO
+void main() {
+    const vec3 bary[3] = {
+        vec3(1.0f, 0.0f, 0.0f),
+        vec3(0.0f, 1.0f, 0.0f),
+        vec3(0.0f, 0.0f, 1.0f)
+    };
+
+    int length = gl_in.length();
+    for (int i = 0; i < length; ++i) {
+        setAttributes(i, bary[i]);
+        EmitVertex();
+    }
+    EndPrimitive();
+}
+
+void setAttributes(int i, vec3 bary) {
+    gl_Position = gl_in[i].gl_Position;
+    attribOut.height = attribIn[i].height;
+    attribOut.texCoords = attribIn[i].texCoords;
+    attribOut.patchDistance = attribIn[i].patchDistance;
+    attribOut.barycentricCoords = bary;
 }
