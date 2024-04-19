d30c59221bafcdef7b37d822348c0530d7a1c62d
Thomas Rouleau <thomas.rouleau@polymtl.ca>
2024-04-19 15:09:21 -0400

    TR | Updated tessellation fragment shader variable names and function declaration order to make reading easier

--- tp4/src/shaders/tessellation.fs.glsl
+++ tp4/src/shaders/tessellation.fs.glsl
@@ -35,31 +35,42 @@ const vec3 PATCH_EDGE_COLOR = vec3(1.0f, 0.0f, 0.0f);
 const float WIREFRAME_WIDTH = 0.5f;
 const float PATCH_EDGE_WIDTH = 0.5f;
 
-void main() {
-    vec4 sandTex = texture(sandSampler, attribIn.texCoords);
-    vec4 groundTex = texture(groundSampler, attribIn.texCoords);
-    vec4 snowTex = texture(snowSampler, attribIn.texCoords);
-
-    FragColor = calculateColor(sandTex, groundTex, snowTex, attribIn.height);
-
-    if (viewWireframe) {
-        applyWireframeEffect();
-    }
-}
-
-vec4 calculateColor(vec4 sandTex, vec4 groundTex, vec4 snowTex, float height) {
+vec4 computeColor(vec4 sandTex, vec4 groundTex, vec4 snowTex, float height) {
+    // height < 0.6 => sand and ground texture for fragment color
+    // height > 0.6 => ground and snow texture for fragment color
     if (height < 0.6f) {
-        float f = smoothstep(0.3f, 0.35f, height);
-        return mix(sandTex, groundTex, f);
+        // height < 0.3f => sand, mixFactor = 0; height > 0.35f => ground, mixFactor = 1; 0.3 < height < 0.35 => Hermite interpolation with smoothstep()
+        float mixFactor = smoothstep(0.3f, 0.35f, height);
+        return mix(sandTex, groundTex, mixFactor);
     } else {
-        float f = smoothstep(0.6f, 0.65f, height);
-        return mix(groundTex, snowTex, f);
+        // height < 0.6f => ground, mixFactor = 0; height > 0.65f => snow, mixFactor = 1; 0.6 < height < 0.65 => Hermite interpolation with smoothstep()
+        float mixFactor = smoothstep(0.6f, 0.65f, height);
+        return mix(groundTex, snowTex, mixFactor);
     }
 }
 
 void applyWireframeEffect() {
-    float edge = edgeFactor(attribIn.barycentricCoords, WIREFRAME_WIDTH);
-    float patchEdge = edgeFactor(attribIn.patchDistance, PATCH_EDGE_WIDTH);
-    FragColor.rgb = mix(FragColor.rgb, WIREFRAME_COLOR, 1 - edge);
-    FragColor.rgb = mix(FragColor.rgb, PATCH_EDGE_COLOR, 1 - patchEdge);
+    // mix final color of textures with wireframe color to show wireframe
+    float edgeMixFactor = edgeFactor(attribIn.barycentricCoords, WIREFRAME_WIDTH);
+    float patchEdgeMixFactor = edgeFactor(attribIn.patchDistance, PATCH_EDGE_WIDTH);
+
+    // mix finale color of textures with patch edge color to show patches edges
+    FragColor.rgb = mix(FragColor.rgb, WIREFRAME_COLOR, 1 - edgeMixFactor);
+    FragColor.rgb = mix(FragColor.rgb, PATCH_EDGE_COLOR, 1 - patchEdgeMixFactor);
+}
+
+void main() {
+    // Get texels for terrain color
+    vec4 sandTex = texture(sandSampler, attribIn.texCoords);
+    vec4 groundTex = texture(groundSampler, attribIn.texCoords);
+    vec4 snowTex = texture(snowSampler, attribIn.texCoords);
+
+    // Find FragColor based on height of vertex
+    FragColor = computeColor(sandTex, groundTex, snowTex, attribIn.height);
+
+    // show wireframe if activated
+    if (viewWireframe) {
+        // Modify FragColor for wireframe
+        applyWireframeEffect();
+    }
 }
